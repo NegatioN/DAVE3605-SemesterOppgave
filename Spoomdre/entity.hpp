c7ae5ff2f632a7d21556a9cd9b09414ecd8adb63
Joakim Rishaug <Joakimrishaug@gmail.com>
2015-04-12 23:43:39 +0200

    added headers for entity, player

---
+++ Spoomdre/entity.hpp
@@ -0,0 +1,15 @@
+#ifndef CLASS_ENTITY_H
+#define CLASS_ENTITY_H
+
+
+
+//Abstract class for entities. Meaning "Player", "Mob", "Moose"
+//should this extend properties? "Position_Vector", "Velocity_vector", "Moveable"
+class Entity{
+
+float xpos;
+float ypos;
+
+};
+
+#endif
---
+++ Spoomdre/player.hpp
@@ -0,0 +1,14 @@
+#ifndef CLASS_GAME_H
+#define CLASS_GAME_H
+
+#include "entity.hpp"
+
+
+//represents the player and everything related
+//should probably contain the user-input in some way.
+class Player : public Entity {
+
+};
+
+
+#endif

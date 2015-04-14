d7559b74cc4a4ae38645a321cdc7c5886d215c2c
Joakim Rishaug <Joakimrishaug@gmail.com>
2015-04-14 23:07:12 +0200

    added header-files for vertex, linedef, sector

---
+++ Spoomdre/sector.hpp
@@ -0,0 +1,15 @@
+#ifndef CLASS_SECTOR_H
+#define CLASS_SECTOR_H
+
+class Sector{
+
+int floor_height_;
+int ceiling_height_;
+//Floor + ceiling textures
+
+public:
+	Sector(int floor_height, int ceiling_height);
+
+};
+
+#endif
---
+++ Spoomdre/vertex.hpp
@@ -0,0 +1,12 @@
+#ifndef CLASS_VERTEX_H
+#define CLASS_VERTEX_H
+
+//vertex is merely a point on our map
+class Vertex{
+int x_;
+int y_;
+public:
+	Vertex(int x, int y);
+};
+
+#endif
